f97061603334f1eb173149631934dfb270c09000
曾文兴 <zengwx@yealink.com>
2022-03-14 14:55:46 +0800

    enable building clang-code-model plugin

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-cmake -DCMAKE_BUILD_TYPE=Debug -DPYTHON_EXECUTABLE=/usr/bin/python3 -G Ninja -DCMAKE_PREFIX_PATH=$HOME/Qt/5.15.2/gcc_64;$HOME/bin/llvm . -B build
+# download pre-built libclang from https://download.qt.io/development_releases/prebuilt/libclang/
+cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python3 -G Ninja -DCMAKE_PREFIX_PATH=/home/bill/Qt/5.15.2/gcc_64:/home/bill/ws_data/libclang/lib/cmake . -B build
+#-DClang_DIR=/home/bill/ws_data/libclang/ 
 cmake --build build
