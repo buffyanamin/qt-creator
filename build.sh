faf38cc6ab37651fa8a11ad0b17433fd1c493f8d
yl4106 <zengwx@yealink.com>
2024-04-26 16:37:42 +0800

    misc: add build environment

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 # download pre-built libclang from https://download.qt.io/development_releases/prebuilt/libclang/
+export Clang_DIR=~/ws_data/libclang/
+export Qt5_DIR=/home/bill/Qt/5.15.2/gcc_64
 cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python3 -G Ninja -DCMAKE_PREFIX_PATH=/home/bill/Qt/5.15.2/gcc_64:/home/bill/ws_data/libclang/lib/cmake . -B build
 #-DClang_DIR=/home/bill/ws_data/libclang/ 
 cmake --build build
